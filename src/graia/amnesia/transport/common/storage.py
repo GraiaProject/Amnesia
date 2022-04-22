01ce6e3d9a8b28366e46179997e488ea943ab635
GreyElaina <GreyElaina@outlook.com>
2022-04-22 18:59:34 +0800

    refactor(builtins): add CacheStorage(from avilla) & Memcache extend it now.
    add `Storage` & `CacheStorage` for cache interface like memcache or redis.

---
+++ src/graia/amnesia/transport/common/storage.py
@@ -0,0 +1,37 @@
+from abc import ABCMeta, abstractmethod
+from datetime import timedelta
+from typing import Any, Generic, List, Optional, TypeVar
+
+from graia.amnesia.launch.service import ExportInterface
+
+D = TypeVar("D")
+
+
+class Storage(ExportInterface):
+    pass
+
+
+class CacheStorage(Storage, Generic[D], metaclass=ABCMeta):
+    @abstractmethod
+    async def get(self, key: str, default: Optional[Any] = None) -> D:
+        ...
+
+    @abstractmethod
+    async def set(self, key: str, value: Any, expire: Optional[timedelta] = None) -> None:
+        ...
+
+    @abstractmethod
+    async def delete(self, key: str, strict: bool = False) -> None:
+        ...
+
+    @abstractmethod
+    async def clear(self) -> None:
+        ...
+
+    @abstractmethod
+    async def has(self, key: str) -> bool:
+        ...
+
+    @abstractmethod
+    async def keys(self) -> List[str]:
+        ...

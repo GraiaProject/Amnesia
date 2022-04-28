731dec3fe6e3e628855ccb72f15f7ce489bd7126
BlueGlassBlock <blueglassblock@outlook.com>
2022-04-28 21:33:20 +0800

    change TransportRegistrar sig

--- src/graia/amnesia/transport/utilles.py
+++ src/graia/amnesia/transport/utilles.py
@@ -10,8 +10,6 @@ if TYPE_CHECKING:
 
 P_TransportHandler = ParamSpec("P_TransportHandler")
 
-R_TransportHandler = TypeVar("R_TransportHandler")
-
 T_Transport = TypeVar("T_Transport", bound="Transport")
 
 
@@ -25,15 +23,15 @@ class TransportRegistrar:
         self.callbacks = {}
         self.declares = []
 
-    def handle(self, signature: TransportSignature[Callable[P_TransportHandler, R_TransportHandler]]):
-        def decorator(method: Callable[Concatenate[T_Transport, P_TransportHandler], R_TransportHandler]):
+    def handle(self, signature: TransportSignature[Callable[P_TransportHandler, Any]]):
+        def decorator(method: Callable[Concatenate[T_Transport, P_TransportHandler], Any]):
             self.handlers[signature] = method
             return method
 
         return decorator
 
-    def on(self, signature: TransportSignature[Callable[P_TransportHandler, R_TransportHandler]]):
-        def decorator(method: Callable[Concatenate[T_Transport, P_TransportHandler], R_TransportHandler]):
+    def on(self, signature: TransportSignature[Callable[P_TransportHandler, Any]]):
+        def decorator(method: Callable[Concatenate[T_Transport, P_TransportHandler], Any]):
             self.callbacks.setdefault(signature, []).append(method)
             return method
 
@@ -43,7 +41,7 @@ class TransportRegistrar:
         self.declares.append(signature)
         return signature
 
-    def apply(self, transport_class: Type["T_Transport"]):
+    def apply(self, transport_class: Type["Transport"]):
         transport_class.handlers.update(self.handlers)
         transport_class.callbacks.update(self.callbacks)
         transport_class.declares.extend(self.declares)

30939338fd44e6d4ebfd990342ba7ad9510f053d
ProgramRipper <programripper@foxmail.com>
2022-08-08 02:34:02 +0800

    chore: format code

--- src/graia/amnesia/builtins/aiohttp.py
+++ src/graia/amnesia/builtins/aiohttp.py
@@ -249,7 +249,9 @@ class AiohttpClientConnectionRider(TransportRider[str, T], Generic[T]):
                         handler = t.get_handler(WebsocketReconnect)
                         tsk = asyncio.create_task(handler(self.status))
                         if not tsk.cancelled():
-                            tsk.add_done_callback(lambda tsk: self.transports.append(t) if tsk.result() is True else None)
+                            tsk.add_done_callback(
+                                lambda tsk: self.transports.append(t) if tsk.result() is True else None
+                            )
                             reconnect_handle_tasks.append(tsk)
                 await wait_fut(reconnect_handle_tasks)
         self.transports = __original_transports
